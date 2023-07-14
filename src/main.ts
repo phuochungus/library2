85711ea7c6a51cfd31006d27ebbbbc8b3992b124
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-07-14 22:10:36 +0700

    add validate config

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Libb example')
