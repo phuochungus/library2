dae71e4b5d06ca06365094c7d48d0d8c2cc615f1
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-07-15 21:07:24 +0700

    move entity to resource folder

--- src/settings/entities/setting.entity.ts
+++ src/settings/entities/setting.entity.ts
@@ -1,1 +1,24 @@
-export class Setting {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Setting {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  value: number;
+
+  @Column({ name: 'feature_description' })
+  featureDescription: string;
+
+  @UpdateDateColumn({ name: 'updated_date' })
+  updatedDate: Date;
+}
